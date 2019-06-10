584b34e377ea43f1da4e44d7ee546e643b44cfab
hiroki-yamada <yamada4tec@gmail.com>
2019-06-10 18:39:54 +0900

    Add: Visual Studio 2019 project.

---
+++ TranslationLibraryGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Cloud.Translation.V2;
+
+namespace GoogleCloudTranslation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+			TranslationClient client = TranslationClient.Create();
+			var response = client.TranslateText("I have an apple.", "ja", "en");
+			Console.WriteLine(response.TranslatedText);
+		}
+	}
+}
